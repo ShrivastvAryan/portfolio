a972a5c8180ea21b5c87605f6eed3061a8d85a24
Aryan <asguy29@gmail.com>
2026-09-13 09:58:21 +0530

    build fixed

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,7 +1,5 @@
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
-
 export const alt = "Aryan Shrivastava | Full Stack Developer & Software Engineer";
 export const size = {
   width: 1200,
